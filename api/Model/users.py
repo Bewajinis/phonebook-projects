e7ab39a98146a833abc3d37c3e8727e453f574df
ADENIYI OLANREWAJU MARK <olanrewaju@speedpay.ng>
2023-08-18 23:12:03 +0100

    check if email is in the right format

--- api/Model/users.py
+++ api/Model/users.py
@@ -1,6 +1,7 @@
 from utils import db
 import uuid
 from sqlalchemy.orm import validates
+import re
 
 
 def generate_hex():
@@ -17,16 +18,23 @@ class User(db.Model):
     password = db.Column(db.String(100), nullable=False)
     phone_book = db.relationship('PhoneBook', backref='user', lazy=True)
 
+    # this is to validate the username
     @validates('username')
+    # declare the function, key is the column name, username is the value
     def validate_username(self, key, username):
+        # if username is not provided, raise an error
         if not username:
             raise AssertionError('No username provided')
+        # if username is already in use, raise an error
         if User.query.filter(User.username == username).first():
             raise AssertionError('Username is already in use')
+        # if username is less than 5 characters or more than 20 characters, raise an error
         if len(username) < 5 or len(username) > 20:
             raise AssertionError('Username must be between 5 and 20 characters')
+        # return the username
         return username
 
+    # this is to validate the email
     @validates('email')
     def validate_email(self, key, email):
         if not email:
@@ -53,7 +61,7 @@ class PhoneBook(db.Model):
     phone_number = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), default='')
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    
+
     def __init__(self, name, phone_number, user_id):
         self.name = name
         self.phone_number = phone_number
@@ -61,3 +69,12 @@ class PhoneBook(db.Model):
 
     def __repr__(self):
         return f'<PhoneBook {self.name}>'
+
+
+# this is to check if the email is in the right format
+def check_for_valid_email(email):
+    # this is to check if the email is valid
+    # this check if the email has @ and .
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return False
+    return True
