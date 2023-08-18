ed68a2a7eab89a6af4ce39c556879fd4474e576b
ADENIYI OLANREWAJU MARK <olanrewaju@speedpay.ng>
2023-08-18 23:43:15 +0100

    auth---logins

--- api/Auth/views.py
+++ api/Auth/views.py
@@ -1,36 +1,93 @@
 from flask_restx import Namespace, Resource, fields
 from flask import request
 from ..Model.users import User
-from werkzeug.security import generate_password_hash, check_password_hash
+from passlib.hash import pbkdf2_sha256 as sha256
 from http import HTTPStatus
-from flask_jwt_extended import create_access_token, create_refresh_token,jwt_required,get_jwt_identity
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    get_jwt_identity,
+)
+from ..func import create_tokens
 
 
-
-auth_namespace = Namespace('auth', description='a namespace for authentication')
+auth_namespace = Namespace("auth", description="a namespace for authentication")
 
 
 signup_model = auth_namespace.model(
-    'SignUp',{
-        'id':fields.Integer(),
-        'username':fields.String(required=True,description='A username'),
-        'email':fields.String(required=True,description="An email"),
-        "password": fields.String(required=True,description="A password")
-    }
+    "SignUp",
+    {
+        "id": fields.Integer(readOnly=True),
+        "username": fields.String(required=True, description="A username"),
+        "email": fields.String(required=True, description="An email"),
+        "first_name": fields.String(required=True, description="A first name"),
+        "last_name": fields.String(required=True, description="A last name"),
+        "password": fields.String(
+            required=True, description="A password", writeOnly=True
+        ),
+        "confirm_password": fields.String(
+            required=True, description="Confirm password", writeOnly=True
+        ),
+    },
 )
 
 
 user_model = auth_namespace.model(
-    "User" ,{
-        'id':fields.Integer(),
-        'username':fields.String(required=True,description="A username"),
-        'email':fields.String(required=True,description="An email"),
-        'password_hash': fields.String(required=True, description='A password'),
-        'is_active':fields.Boolean(description= 'This shows that User is active')
-    }
+    "User",
+    {
+        "id": fields.Integer(),
+        "username": fields.String(required=True, description="A username"),
+        "email": fields.String(required=True, description="An email"),
+        "is_active": fields.Boolean(description="This shows that User is active"),
+    },
+)
+
+login_model = auth_namespace.model(
+    "Login",
+    {
+        "username": fields.String(required=True, description="A username"),
+        "password": fields.String(
+            required=True, description="A password", writeOnly=True
+        ),
+    },
 )
 
 
-@auth_namespace.route('/signup')
+@auth_namespace.route("/signup")
 class Signup(Resource):
-    
+    @auth_namespace.expect(signup_model)
+    @auth_namespace.response(
+        HTTPStatus.CREATED, "User created successfully", user_model
+    )
+    # @auth_namespace.response(HTTPStatus.BAD_REQUEST, "Bad request")
+    # @auth_namespace.response(HTTPStatus.CONFLICT, "Username or email already exists")
+    def post(self):
+        data = request.get_json()
+        data["username"] = data["username"].lower()
+        data["email"] = data["email"].lower()
+
+        if data["password"] != data["confirm_password"]:
+            auth_namespace.abort(HTTPStatus.BAD_REQUEST, "Passwords do not match")
+        user = User(**data)
+        user.save()
+        return user, HTTPStatus.CREATED
+
+
+@auth_namespace.route("/login")
+class Login(Resource):
+    @auth_namespace.expect(login_model)
+    @auth_namespace.response(HTTPStatus.OK, "User logged in successfully", user_model)
+    @auth_namespace.response(HTTPStatus.UNAUTHORIZED, "Invalid credentials")
+    def post(self):
+        data = request.get_json()
+        username = data["username"].lower()
+        password = data["password"]
+        user = User.query.filter_by(username=username).first()
+        if user and sha256.verify(password, user.password):
+            return (
+                create_tokens(user),
+                HTTPStatus.OK,
+            )
+        else:
+            auth_namespace.abort(HTTPStatus.UNAUTHORIZED, "Invalid credentials")
