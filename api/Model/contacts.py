28363da9d5d57b429090e2e48ee3bdabf42c6734
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2023-08-18 23:56:48 +0100

    Update contacts.py

---
+++ api/Model/contacts.py
@@ -0,0 +1,23 @@
+from utils import db
+from users import generate_hex
+
+
+class Contacts(db.Model):
+    __tablename__ = 'contacts'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, default=generate_hex())
+    name = db.Column(db.String(50), nullable=False)
+    phone_number = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), default='')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __init__(self, name, phone_number, user_id):
+        self.name = name
+        self.phone_number = phone_number
+        self.user_id = user_id
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'<PhoneBook {self.name}>'
